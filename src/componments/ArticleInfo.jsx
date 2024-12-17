33238a9344ed0382522b2bc4aba84c6888d01f4c
Lewis Feely <LewisFeely@Family-MacBook-Air.local>
2024-12-17 16:14:03 +0000

    adding votes

--- src/componments/ArticleInfo.jsx
+++ src/componments/ArticleInfo.jsx
@@ -4,6 +4,7 @@ import { data, Link } from "react-router-dom";
 
 function ArticleInfo({ article_id }) {
   const [article, setArticle] = useState({});
+  const [articleVotes, setArticleVotes] = useState(0);
   function reset() {
     setArticle({});
   }
@@ -11,7 +12,25 @@ function ArticleInfo({ article_id }) {
     api.get(`/api/articles/${article_id}`).then(({ data }) => {
       setArticle(data.response[0]);
     });
-  }, []);
+  }, [article]);
+  function dropVote(e) {
+    setArticleVotes(article.votes);
+    const request = {
+      inc_votes: 1,
+    };
+    api.patch(`/api/articles/${article_id}`, request).then(({ data }) => {
+      setArticle(data.updatedArticle);
+    });
+  }
+  function downVote(e) {
+    setArticleVotes(article.votes);
+    const request = {
+      inc_votes: -1,
+    };
+    api.patch(`/api/articles/${article_id}`, request).then(({ data }) => {
+      setArticle(data.updatedArticle);
+    });
+  }
   return (
     <>
       <h4 className="author-name">author: {article.author}</h4>
@@ -21,6 +40,14 @@ function ArticleInfo({ article_id }) {
         <p>topic: {article.topic}</p>
 
         <p className="author-name">{article.body}</p>
+        <p>votes: {article.votes}</p>
+        <button onClick={dropVote} className="button-18">
+          increment vote
+        </button>
+        <button onClick={downVote} className="button-18">
+          decrease vote
+        </button>
+        <br />
         <Link to="/api/articles/:article_id/comments" className="link">
           view articles {article.comment_count} comments
         </Link>
